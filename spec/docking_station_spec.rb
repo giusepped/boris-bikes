32f19fe8d77755cb4b34dcbd3f4e63f3e8eb7c82
giuseppe de santis <giusdesan@gmail.com>
2015-07-23 22:11:25 +0100

    substituted Bike.new with :bike in docking station spec" "

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -15,8 +15,8 @@ describe DockingStation do
     expect(bike).to be_working
   end
   it "only accepts bikes up to a max capacity" do
-    subject.capacity.times {subject.dock Bike.new}
-    expect{subject.dock Bike.new}.to raise_error "Station is full"
+    subject.capacity.times {subject.dock :bike}
+    expect{subject.dock :bike}.to raise_error "Station is full"
   end
 
 
